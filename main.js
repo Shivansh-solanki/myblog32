cb53b3164674d7920f2e6a39833de2515ebb0370
Shivansh-solanki <94776256+Shivansh-solanki@users.noreply.github.com>
2023-05-31 14:44:48 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,24 @@
+function setup() {
+    video = createCapture(VIDEO);
+    video.size(550,500);
+    canvas = createCanvas(550,500);
+    canvas.position(560,150);
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', gotPoses);
+}
+
+function modelLoaded() {
+    console.log('PoseNet Is Initialized');
+}
+
+function draw() {
+    background('#FFFF00');
+}
+
+function gotPoses(results)
+{
+    if(results.lenght > 0)
+    {
+        console.log(results);
+    }
+}
